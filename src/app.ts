6d14ecd948f95e11ac3d37b49d2741722dc640c6
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2025-10-01 19:34:30 +0700

    cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import loggerReqMiddleware from "@/middlewares/logger-req.middleware";
 import sessionRoute from "@/routes/session.route";
 import factory from "@/lib/factory";
 import {serveStatic} from "@hono/node-server/serve-static";
+import {cors} from "hono/cors";
 
 const app = factory.createApp()
 
+app.use("*", cors())
 app.use("*", loggerReqMiddleware)
 app.use("/uploads/*", serveStatic({root: "./"}))
 
